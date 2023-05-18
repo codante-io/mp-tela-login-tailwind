e8fb3098dcc121b2684db8feabf1debe681d8cb2
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-05-18 10:29:59 -0300

    feat: windows desktop login complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
+import ContainerLogin from '@/components/ContainerLogin'
+import HeroLogin from '@/components/HeroLogin'
+import Login from '@/components/Login'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,12 +9,15 @@ export default function Home() {
   return (
     <main
       className={`
-        flex min-h-screen flex-col 
+        flex min-h-screen flex-col min-w-screen
         items-center justify-center p-24 ${inter.className}
-        bg-gradient-to-r from-black to-gray-950
+        bg-gradient-to-r from-zinc-950 to-zinc-900
       `}
     >
-      <h1>MP Login</h1>
+      <ContainerLogin>
+        <Login />
+        <HeroLogin />
+      </ContainerLogin>
     </main>
   )
 }
