e51bc6e74c14270154d1a5571723bedc4e51dc98
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-19 18:12:31 -0300

    :sparkles: feat: adicionando inputs na página Login

--- loginPage/src/pages/Login.jsx
+++ loginPage/src/pages/Login.jsx
@@ -1,12 +1,16 @@
+import Input from '../components/Input';
+
 function Login() {
   return (
-    <section className="w-5/6 h-5/6 bg-black m-auto">
-      <div>
-        <h1>
+    <section className="w-5/6 h-5/6 bg-black m-auto rounded-2xl">
+      <div className="flex ">
+        <h1 className="text-4xl text-white font-semibold">
           Faça seu login
-          <span>
+          <span className="text-fuchsia-400 text-5xl">
             .
           </span>
+          <Input />
+          <Input />
         </h1>
       </div>
     </section>
