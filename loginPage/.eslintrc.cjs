adb405b5865eba1a08189d6b29936ee639e0b81c
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-19 17:28:47 -0300

    :rotating_light: lint: corringinndo erros de lint

--- loginPage/.eslintrc.cjs
+++ loginPage/.eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    '@trybe/eslint-config-frontend',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
@@ -12,4 +13,4 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
-}
+};
--- loginPage/tailwind.config.js
+++ loginPage/tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
