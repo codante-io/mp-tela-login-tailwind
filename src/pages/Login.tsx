99315ac3f5dec2574186b1649302575258ef2c19
Luis Fernando <luis.silva9902@gmail.com>
2023-05-20 12:06:36 -0300

    Creating uncontrolled form validation without using states and libraries (often used to create controlled forms)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,48 @@
+import { FormEvent } from 'react'
 import Banner from '../assets/banner.png'
 
 export function Login() {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const emailIsValid = validateInput('email', event.currentTarget.email.value)
+    const passwordIsValid = validateInput(
+      'password',
+      event.currentTarget.password.value,
+    )
+
+    if (!emailIsValid) {
+      showWarningMessage('email')
+    }
+
+    if (!passwordIsValid) {
+      showWarningMessage('password')
+    }
+  }
+
+  function validateInput(name: string, value: string) {
+    switch (name) {
+      case 'email': {
+        return /^[\w.-]+@[\w-]+\.[\w.-]{2,}$/.test(value)
+      }
+
+      case 'password': {
+        return /^.{8,}$/.test(value)
+      }
+    }
+  }
+
+  function showWarningMessage(name: string) {
+    const warning = document.getElementById(`warning-${name}`)
+    if (warning) {
+      warning.classList.remove('hidden')
+
+      setTimeout(() => {
+        warning.classList.add('hidden')
+      }, 1500)
+    }
+  }
+
   return (
     <div className="flex h-screen items-center justify-center bg-gradient-to-r font-body text-gray-400">
       <div className="flex w-[1106px] justify-between overflow-hidden rounded-[34px] bg-black shadow-form">
@@ -9,25 +51,39 @@ export function Login() {
             Faça seu login
           </h1>
 
-          <form className="mt-14 flex flex-col">
+          <form className="mt-14 flex flex-col" onSubmit={handleSubmit}>
             <label className="text-sm text-gray-400">
               Email
               <div className="mt-2 w-fit rounded-xl p-[1px] transition-all focus-within:bg-eclipse-10 focus-within:shadow-input">
                 <input
-                  type="text"
+                  type="email"
+                  id="email"
                   className="w-[355px] rounded-xl bg-neutral-900 p-3 text-base focus-within:outline-0"
                 />
               </div>
+              <span
+                className="absolute hidden text-xs text-red-500"
+                id="warning-email"
+              >
+                E-mail inválido
+              </span>
             </label>
 
             <label className="mt-6 text-sm text-gray-400">
               Senha
               <div className="mt-2 w-fit rounded-xl p-[1px] transition-all focus-within:bg-eclipse-10 focus-within:shadow-input">
                 <input
-                  type="text"
+                  type="password"
+                  id="password"
                   className="w-[355px] rounded-xl bg-neutral-900 p-3 text-base focus-within:outline-0"
                 />
               </div>
+              <span
+                className="absolute hidden text-xs text-red-500"
+                id="warning-password"
+              >
+                Senha inválida (mínimo 8 caracteres)
+              </span>
             </label>
 
             <a
