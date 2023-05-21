37d20772dc9e5b48156ca09a4798fb4a620d9ef4
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-05-21 19:36:59 -0300

    falta o efeito de focus

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,32 +1,35 @@
 export default function Login() {
   return (
-    <div className="bg-login-bg flex w-full md:w-1/2 lg:w-1/4">
+    <div className="bg-login-bg flex w-full md:w-1/2 lg:w-1/4 lg:h-3/6">
 
       <section>
         <h1 className="text-4xl text-gray-200 m-10">
           Faça seu login
           <span className="bg-gradient-to-r from-custom-point-gradient-0 via-custom-point-gradient-1 to-custom-point-gradient-2 text-transparent bg-clip-text">.</span>
         </h1>
-        <form className="flex flex-col gap-4 m-10">
-          <label htmlFor="email" className="text-gray-400">
+        <form className="flex flex-col gap-1 m-10">
+          <label htmlFor="email" className="text-gray-400 text-sm">
             Email
           </label>
-            <input type="text" name="" id="email" className="bg-input rounded-md text-gray-400" />
-          <label htmlFor="password" className="text-gray-400">
+            <input type="text" name="" id="email" className="bg-input rounded-md text-gray-400 w-2/2 h-10  mb-5" />
+          <label htmlFor="password" className="text-gray-400 text-sm">
             Senha
           </label> 
-            <input type="text"  id="password" className="bg-input rounded-md text-gray-400"/>
-          <p className="text-gray-400 underline flex justify-end">Esqueci minha senha</p>
+            <input type="text"  id="password" className="bg-input rounded-md text-gray-400 w-2/2 h-10 mb-5"/>
+          <p className="text-gray-400 underline flex justify-end text-sm mb-5">Esqueci minha senha</p>
           <button type="button" className="bg-gradient-to-tr from-custom-point-gradient-0 via-custom-point-gradient-1 to-custom-point-gradient-2
           rounded-md
           text-white
+          w-2/2
+          h-10
+          mb-5
           ">
             Entrar
           </button>
-          <p className="text-gray-400 underline flex justify-center">Ainda não tenho uma conta</p>
+          <p className="text-gray-400 underline flex justify-center text-sm">Ainda não tenho uma conta</p>
         </form>
       </section>
-      <img className="w-1/2 rounded-md" src="/src/assets/image1.png" alt="oi" />
+      <img className="w-1/2 rounded-md w-full" src="/src/assets/image1.png" alt="oi" />
     </div>
   )
 }
