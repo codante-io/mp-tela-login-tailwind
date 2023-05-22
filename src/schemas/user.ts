ae5b3b2d6bef70f30ef5c1ca6c31577243e73255
Theus <matheusth.dev@gmail.com>
2023-05-21 23:06:31 -0300

    feat: schema de validação de campos de recuperação de senha

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -43,3 +43,31 @@ export const userCreateSchema = z
     message: "Senhas não são iguais",
     path: ["confirmPassword"],
   });
+
+export const userRecoverSchema = z
+  .object({
+    email: z
+      .string()
+      .nonempty("campo obrigatório")
+      .regex(/^\S*$/, "não pode haver espaços em branco")
+      .email("digite um e-mail válido"),
+
+    newPassword: z
+      .string()
+      .nonempty("campo obrigatório")
+      .regex(/^\S*$/, "não pode haver espaços em branco")
+      .regex(/[A-Z]/, "deve conter ao menos 1 letra maiúscula")
+      .regex(/([a-z])/, "deve conter ao menos 1 letra minúscula")
+      .regex(/(\d)/, "deve conter ao menos 1 número")
+      .regex(/(\W)/, "deve conter ao menos 1 caracter especial")
+      .min(8, "no mínimo 8 caracteres"),
+
+    confirmNewPassword: z.string().nonempty("campo obrigatório"),
+  })
+  .refine(
+    ({ newPassword, confirmNewPassword }) => newPassword === confirmNewPassword,
+    {
+      message: "Senhas não são iguais",
+      path: ["confirmNewPassword"],
+    }
+  );
