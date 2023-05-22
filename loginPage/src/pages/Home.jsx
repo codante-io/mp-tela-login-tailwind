8ea47e1415ec87187b14d7dbc6e324130faa96f4
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-22 16:58:36 -0300

    :sparkles: feat: adicionando email na pagina principal

--- loginPage/src/pages/Home.jsx
+++ loginPage/src/pages/Home.jsx
@@ -1,6 +1,12 @@
+import { useContext } from 'react';
+import LoginContext from '../context/LoginContext';
+
 function Home() {
+  const { email } = useContext(LoginContext);
   return (
-    <h1>Welcome to the Codante Challange </h1>
+    <h1>
+      { `Hello ${email}`}
+    </h1>
   );
 }
 
