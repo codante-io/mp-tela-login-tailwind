63f407e8c88df577de185351f26c01f588a4ac23
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-22 18:10:06 -0300

    :sparkles: feat: criando variaveis

--- loginPage/src/components/LoginButton.jsx
+++ loginPage/src/components/LoginButton.jsx
@@ -1,11 +1,24 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import LoginContext from '../context/LoginContext';
 
 function LoginButton() {
+  const { email,
+    setIsNull,
+    password } = useContext(LoginContext);
   const navigate = useNavigate();
+  const handleClick = () => {
+    if (email && password) {
+      navigate('home');
+    }
+    setIsNull(true);
+  };
   return (
     <button
       type="submit"
-      onClick={ () => navigate('home') }
+      className="bg-gradient-to-r from-violet-500 via-pink-500 to-rose-200
+      h-11 rounded-xl text-lg"
+      onClick={ handleClick }
     >
       Entrar
     </button>
--- loginPage/src/context/LoginProvider.jsx
+++ loginPage/src/context/LoginProvider.jsx
@@ -3,12 +3,21 @@ import PropTypes from 'prop-types';
 import LoginContext from './LoginContext';
 
 function LoginProvider({ children }) {
-  const [email, setEmail] = useState([]);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isNull, setIsNull] = useState(false);
+  const [emailValid, setEmailValid] = useState(false);
 
   const contextLogin = useMemo(() => ({
     email,
     setEmail,
-  }), [email]);
+    password,
+    setPassword,
+    isNull,
+    setIsNull,
+    emailValid,
+    setEmailValid,
+  }), [email, isNull, emailValid, password]);
   return (
     <LoginContext.Provider value={ contextLogin }>
       { children }
