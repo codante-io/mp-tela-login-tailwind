b57e49c007f5c17dd0163447e5ddadb90eb3f57f
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-22 16:58:12 -0300

    :sparkles: feat: criando função para capturar  login

--- loginPage/src/components/InputUsername.jsx
+++ loginPage/src/components/InputUsername.jsx
@@ -1,8 +1,21 @@
+import { useContext } from 'react';
+import LoginContext from '../context/LoginContext';
+
 function InputUsername() {
+  const { setEmail } = useContext(LoginContext);
+  const handlechange = (x) => {
+    console.log(x.value);
+    setEmail(x.value);
+  };
+
   return (
     <label className="flex flex-col">
       Email
-      <input type="text" className="rounded-2xl h-16" />
+      <input
+        type="text"
+        className="rounded-2xl h-16"
+        onChange={ (e) => handlechange(e.target) }
+      />
     </label>
   );
 }
