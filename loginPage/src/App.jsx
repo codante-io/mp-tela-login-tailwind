d8943a9c287823a8613af05a965353a885187ffb
Jeferson Gomes <jeferson_gjr@hotmail.com>
2023-05-22 16:57:54 -0300

    :sparkles: feat: adicionando provider

--- loginPage/src/App.jsx
+++ loginPage/src/App.jsx
@@ -2,18 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import LoginProvider from './context/LoginProvider';
 
 function App() {
   return (
-    <Router>
-      <section className="flex justify-center h-screen bg-slate-900">
-        <Routes>
-          <Route path="/" element={ <Login /> } />
-          <Route path="/home" element={ <Home /> } />
-          <Route path="*" element={ <NotFound /> } />
-        </Routes>
-      </section>
-    </Router>
+    <LoginProvider>
+      <Router>
+        <section className="flex justify-center h-screen bg-slate-900">
+          <Routes>
+            <Route path="/" element={ <Login /> } />
+            <Route path="/home" element={ <Home /> } />
+            <Route path="*" element={ <NotFound /> } />
+          </Routes>
+        </section>
+      </Router>
+    </LoginProvider>
   );
 }
 
