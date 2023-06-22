ac817522ac054f84081c4240b6afbc157beedc69
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2023-06-22 15:13:15 -0300

    alterando input e criando logica de redirecionamento

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Login from './components/Login';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className='bg-gradient-to-br from-custom-gradient-0 via-custom-gradient-1 to-custom-gradient-2 flex items-center justify-center h-screen' >
-      <Login />
+    <Routes>
+      <Route exact path='/' element={<Login />} />
+      <Route exact path='/welcome' element={<img className='h-full' src='/src/assets/welcome-greetings.gif'/> } />
+    </Routes>
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,6 @@
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const navigate = useNavigate();
   return (
     <div className="bg-login-bg flex lg:w-1/4 lg:flex-row md:w-1/2 sm:flex-col">
 
@@ -7,7 +9,11 @@ export default function Login() {
           Faça seu login
           <span className="bg-gradient-to-r from-custom-point-gradient-0 via-custom-point-gradient-1 to-custom-point-gradient-2 text-transparent bg-clip-text">.</span>
         </h1>
-        <form className="flex flex-col gap-1 m-10">
+        <form className="flex flex-col gap-1 m-10" onSubmit={ (e) => {
+           e.preventDefault()
+           navigate('/welcome')
+          }
+          }>
           <label htmlFor="email" className="text-gray-400 text-sm">
             Email
           </label>
@@ -15,7 +21,7 @@ export default function Login() {
             <div className=" transition ease-in opacity-0 group-focus-within:opacity-100 bg-gradient-to-tr from-custom-point-gradient-0 via-custom-point-gradient-1 to-custom-point-gradient-2 h-10 blur-md rounded-lg">
             </div>
               <div className="relative -top-[2.5rem] p-1 bg-none focus-within:bg-gradient-to-tr from-custom-point-gradient-0 via-custom-point-gradient-1 to-custom-point-gradient-2 rounded-lg">
-                <input type="text" name="" id="email" className="bg-input rounded-md text-gray-400 h-10 " />
+                <input type="text" name="email" id="email" required className="bg-input rounded-md text-gray-400 h-10 p-1" />
               </div>
           </div>
 
@@ -26,17 +32,19 @@ export default function Login() {
             <div className=" transition ease-in opacity-0 group-focus-within:opacity-100 bg-gradient-to-tr from-custom-point-gradient-0 via-custom-point-gradient-1 to-custom-point-gradient-2 h-10 blur-md rounded-lg">
             </div>
               <div className="relative -top-[2.5rem] p-1 bg-none focus-within:bg-gradient-to-tr from-custom-point-gradient-0 via-custom-point-gradient-1 to-custom-point-gradient-2 rounded-lg">
-                <input type="text" name="" id="email" className="bg-input rounded-md text-gray-400 h-10 " />
+                <input type="password" name="" id="password" required className="bg-input rounded-md text-gray-400 h-10 p-1" />
               </div>
           </div>
           <p className="text-gray-400 underline flex justify-end text-sm mb-5">Esqueci minha senha</p>
-          <button type="button" className="bg-gradient-to-tr from-custom-point-gradient-0 via-custom-point-gradient-1 to-custom-point-gradient-2
+          <button type="submit" className="bg-gradient-to-tr from-custom-point-gradient-0 via-custom-point-gradient-1 to-custom-point-gradient-2
           rounded-md
           text-white
           w-2/2
           h-10
           mb-5
-          ">
+          "
+          
+          >
             Entrar
           </button>
           <p className="text-gray-400 underline flex justify-center text-sm">Ainda não tenho uma conta</p>
